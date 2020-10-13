5e5374ce4c2030aa429c1c923133a0052d52ead8
kiyoesjh <hyeyaa125@gmail.com>
2020-10-13 17:44:15 +0900

    feat: login 기능 추가

--- src/Routes/Auth.js
+++ src/Routes/Auth.js
@@ -1,5 +1,66 @@
-import React from "react";
+import { authService } from 'fbase';
+import React, { useState } from 'react';
 
-const Auth = () => <span>Auth</span>;
+const Auth = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [newAccount, setNewAccount] = useState(true);
+  const [error, setError] = useState('');
+  const onChange = ({ target: { name, value } }) => {
+    if (name === 'email') {
+      setEmail(value);
+    } else if (name === 'password') {
+      setPassword(value);
+    }
+  };
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      if (newAccount) {
+        //계정만들기
+        await authService.createUserWithEmailAndPassword(email, password);
+      } else {
+        //로그인
+        await authService.signInWithEmailAndPassword(email, password);
+      }
+    } catch (err) {
+      setError(err.message);
+    }
+  };
+
+  const toggleAccount = () => setNewAccount((prev) => !prev);
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          name="email"
+          type="email"
+          placeholder="Email"
+          required
+          value={email}
+          onChange={onChange}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          required
+          value={password}
+          onChange={onChange}
+        />
+        <input type="submit" value={newAccount ? '회원가입' : '로그인'} />
+      </form>
+      <div>
+        <button type="button" onClick={toggleAccount}>
+          {newAccount ? '로그인' : '회원가입'}
+        </button>
+        <button type="button">Google로 시작하기</button>
+        <button type="button">Github로 시작하기</button>
+      </div>
+      <p>{error}</p>
+    </div>
+  );
+};
 
 export default Auth;
