fbada086cdfd62818ca2fbf2a901242442bd1a33
kiyoesjh <hyeyaa125@gmail.com>
2020-10-14 23:36:01 +0900

    feat: Home Component 폼 추가

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -1,5 +1,28 @@
-import React from "react";
+import React, { useState } from 'react';
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+  const [write, setWrite] = useState('');
+  const onSubmit = (event) => {
+    event.preventDefault();
+    const {
+      target: { value },
+    } = event;
+  };
+  const onChange = ({ target: { value } }) => setWrite(value);
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          onChange={onChange}
+          value={write}
+          placeholder="what's on your mind?"
+          maxLength={128}
+        />
+        <input type="submit" value="ssok" />
+      </form>
+    </div>
+  );
+};
 
 export default Home;
