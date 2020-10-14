3f5cf1722f643b2099298078757b3f95946acdd5
kiyoesjh <hyeyaa125@gmail.com>
2020-10-15 00:52:28 +0900

    feat: 글 작성, 가져오기 기능 추가

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -1,8 +1,22 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { dbService } from 'fbase';
 
 const Home = () => {
   const [ssok, setSsok] = useState('');
+  const [ssoks, setSsoks] = useState([]);
+  const getSsoks = async () => {
+    const dbSsoks = await dbService.collection('ssok').get();
+    dbSsoks.forEach((document) => {
+      const ssokObject = {
+        ...document.data(),
+        id: document.id,
+      };
+      setSsoks((prev) => [ssokObject, ...prev]);
+    });
+  };
+  useEffect(() => {
+    getSsoks();
+  }, []);
   const onSubmit = async (event) => {
     event.preventDefault();
     await dbService.collection('ssok').add({
@@ -13,18 +27,25 @@ const Home = () => {
   };
   const onChange = ({ target: { value } }) => setSsok(value);
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input
-          type="text"
-          onChange={onChange}
-          value={ssok}
-          placeholder="what's on your mind?"
-          maxLength={128}
-        />
-        <input type="submit" value="ssok" />
-      </form>
-    </div>
+    <>
+      <div>
+        <form onSubmit={onSubmit}>
+          <input
+            type="text"
+            onChange={onChange}
+            value={ssok}
+            placeholder="what's on your mind?"
+            maxLength={128}
+          />
+          <input type="submit" value="ssok" />
+        </form>
+      </div>
+      <div>
+        {ssoks.map(({ ssok, id, createdAt }) => (
+          <div key={id}>{ssok}</div>
+        ))}
+      </div>
+    </>
   );
 };
 
