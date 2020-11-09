575b73fba21471b098aadc5cedf24ee82c309de6
kiyoesjh <hyeyaa125@gmail.com>
2020-11-10 00:54:15 +0900

    Updates

---
+++ precache-manifest.26313263aa142620a3aa86f65df388cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64a1a394e244d5cc143fa9df8b25ef0a",
+    "url": "/ssok-ddak/index.html"
+  },
+  {
+    "revision": "2c39db939a37a04b2bfe",
+    "url": "/ssok-ddak/static/js/2.1d5c3f00.chunk.js"
+  },
+  {
+    "revision": "8e844923efaee6906e00c8fde9e55716",
+    "url": "/ssok-ddak/static/js/2.1d5c3f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aeae78c8260835b12dc8",
+    "url": "/ssok-ddak/static/js/main.53e90eea.chunk.js"
+  },
+  {
+    "revision": "cd2f88b529b9cd9e2d51",
+    "url": "/ssok-ddak/static/js/runtime-main.7e820de9.js"
+  },
+  {
+    "revision": "415dfbf4ab2df060fe2d9eade2824767",
+    "url": "/ssok-ddak/static/media/DancingScript-SemiBold.415dfbf4.ttf"
+  },
+  {
+    "revision": "d6425aff7a6c668739ea0383aed74b38",
+    "url": "/ssok-ddak/static/media/RIDIBatang.d6425aff.otf"
+  }
+]);
